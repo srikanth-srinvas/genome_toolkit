28065fd0aa079443fce24bbcb00fc8ecc2085654
srikanth-srinivas <srikanth.2001@outlook.com>
2024-05-21 14:10:06 +0530

    updated comments for the count_kmer algorithm

--- genome_toolkit.py
+++ genome_toolkit.py
@@ -4,7 +4,17 @@ class genomeToolkit:
         print("[Genome Toolkit Initiated]\n")
 
     def count_kmer(self, sequence, kmer):
-        """Counts repeating k-mers in a sequence. Includes overlapping k-mers"""
+"""
+        Counts the number of times a specific k-mer appears in a given sequence,
+        including overlapping k-mers.
+
+        Parameters:
+            sequence (str): The DNA sequence to search in.
+            kmer (str): The specific k-mer to search for in the sequence.
+
+        Returns:
+            int: The number of times the k-mer appears in the sequence.
+        """
         kmer_count = 0
         for position in range(len(sequence) - (len(kmer) - 1)):
             if sequence[position:position + len(kmer)] == kmer:
