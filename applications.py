923734e5fa020d69d71971b75165d8224cb69839
srikanth-srinivas <srikanth.2001@outlook.com>
2024-05-21 13:25:42 +0530

    added function to count k-mers

--- applications.py
+++ applications.py
@@ -1,3 +1,10 @@
 from genome_toolkit import genomeToolkit
 
 gt = genomeToolkit()
+
+seq = "AAAGAAAATTGA"
+kmer = "AA"
+
+print(f'Sequence: {seq}')
+print(f'k-mer: {kmer}')
+print(f'Repeats found: {gt.count_kmer(seq, kmer)}')
--- genome_toolkit.py
+++ genome_toolkit.py
@@ -1,3 +1,12 @@
 class genomeToolkit:
     def __init__(self):
         print("Genome toolkit initialised")
+        print("[Genome Toolkit Initiated]\n")
+
+    def count_kmer(self, sequence, kmer):
+        """Counts repeating k-mers in a sequence. Includes overlapping k-mers"""
+        kmer_count = 0
+        for position in range(len(sequence) - len(kmer) + 1):
+            if sequence[position:position + len(kmer)] == kmer:
+                kmer_count += 1
+        return kmer_count
