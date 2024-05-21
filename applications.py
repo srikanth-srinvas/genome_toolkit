3c1a3c19b5a4081c30684dcfce2949e81285bdca
srikanth-srinivas <srikanth.2001@outlook.com>
2024-05-21 14:26:21 +0530

    added print statements to test most_frequent_kmers

--- applications.py
+++ applications.py
@@ -2,9 +2,11 @@ from genome_toolkit import genomeToolkit
 
 gt = genomeToolkit()
 
-seq = "AATTAAAC"
-kmer = "AAA"
+seq = "AATTTTAAAAC"
+kmer = "AA"
+k_len = 3
 
 print(f'Sequence: {seq}')
 print(f'k-mer: {kmer}')
 print(f'Repeats found: {gt.count_kmer(seq, kmer)}')
+print(f'Most frequent k-mer: {gt.find_most_frequent_kmers(seq, k_len)}')
